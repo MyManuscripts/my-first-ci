9d67ce73edf68c92f965e47a22e6fcb8df88bba1
DmitryKulikov-it <dmitrykulikov99@gmail.com>
2025-10-12 12:50:19 +0300

    feat: add main for Calculator

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -8,4 +8,11 @@ public class Calculator {
     public int subtract (int x, int y){
         return x-y;
     }
+    // Добавим main-метод для запуска
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int result = calc.add(10, 20);
+        System.out.println("Result: " + result);
+        System.out.println("Java app is running!");
+    }
 }
